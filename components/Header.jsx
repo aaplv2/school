58db9fed703803e5550ee64b6d91c406f036e1c7
Andres <aaplv2@gmail.com>
2024-11-29 11:29:51 -0300

    MobileNav y cambios Sheet

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 // components
 import Nav from "./Nav";
-import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -24,7 +25,9 @@ const Header = () => {
         </div>
 
         {/* mobile nav */}
-        <div className="xl:hidden">mobile nav</div>
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
