b4cfe9f4cb1b8dadc5590764254b596cd6bdd270
Andres <aaplv2@gmail.com>
2024-11-29 16:58:26 -0300

    imagen

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Pequeño subtítulo</span>
             <h1 className="h1 mb-6">
               Hola <br />
@@ -40,7 +40,7 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
