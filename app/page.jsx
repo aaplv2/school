6a2672a9122ccb2d15b6309d5a0b8203dbec7232
Andres <aaplv2@gmail.com>
2024-11-29 16:44:52 -0300

    Trabajo en homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,52 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+
+// components
+import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Pequeño subtítulo</span>
+            <h1 className="h1 mb-6">
+              Hola <br />
+              <span className="text-accent">Nombre de escuela</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Breve descripción de la escuela, a manera que resuma lo que hace y
+              un par de caracteristicas, no debe ser mayor a tres lineas
+            </p>
+            {/* boton y redes */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Tomar clases</span>
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center 
+                  items-center text-accent text-base hover:bg-accent hover:text-primary 
+                  hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
-            Escuela de Swing<span className="text-accent">.</span>
+            Nombre<span className="text-accent">.</span>
           </h1>
         </Link>
 
