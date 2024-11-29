b76589e1975f13448a243235403a5bc1e148b924
Andres <aaplv2@gmail.com>
2024-11-29 10:48:22 -0300

    Transicion escalones

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,5 +1,52 @@
+import { motion } from "framer-motion";
+
+// variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+// Cálculo del index revertido para el delay escalonado.
+const reverseIndex = (index) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
+
 const Stairs = () => {
-  return <div>Stairs</div>;
+  return (
+    <>
+      {/* 
+        Renderizar 6 motion divs, cada uno representando un peldaño.
+        Cada div tendrá la misma animación definida por el objeto stairAnimation.
+        The el delay en cada div se calcula dinámicamente en base a su index revertido,
+        creando un effecto escalonado con delay para cada peldaño subsecuente.
+    */}
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default Stairs;
