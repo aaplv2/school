206bd6ae56097486a4b40967183f2e4f351681b1
Andres <aaplv2@gmail.com>
2024-12-02 12:35:57 -0300

    animación círculo

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -28,10 +28,10 @@ const Photo = () => {
               ease: "easeInOut",
             },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+          className="w-[250px] h-[250px] xl:w-[450px] xl:h-[450px] mix-blend-lighten absolute ml-7 mt-6"
         >
           <Image
-            src="/assets/photo.png"
+            src="/assets/logo_parenteswing1_transparente.png"
             priority
             quality={100}
             fill
@@ -41,6 +41,32 @@ const Photo = () => {
         </motion.div>
 
         {/* círculo */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#fa9f18"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
