8fec941f743127b608c9c421561a9230555b43fc
Andres <aaplv2@gmail.com>
2024-12-04 18:35:04 -0300

    pagina de cursos ok

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -66,17 +66,22 @@ const Courses = () => {
               >
                 {/* top */}
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
                     {course.num}
                   </div>
-                  <Link href={course.href}>
-                    <BsArrowDownRight />
+                  <Link
+                    href={course.href}
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
                 {/* encabezado */}
-                <h2>{course.title}</h2>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {course.title}
+                </h2>
                 {/* descripción */}
-                <p>{course.description}</p>
+                <p className="text-white/60">{course.description}</p>
                 {/* borde */}
                 <div className="border-b border-white/20 w-full"></div>
               </div>
