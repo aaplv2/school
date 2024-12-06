3c4b7b793de79e14735b608fbbe842fef82cdb31
Andres <aaplv2@gmail.com>
2024-12-06 12:55:45 -0300

    school page

--- app/school/page.jsx
+++ app/school/page.jsx
@@ -27,7 +27,7 @@ const about = {
   ],
 };
 
-// us data
+// experiencia data
 const experience = {
   icon: "/assets/resume/badge.svg",
   title: "Nuestra Escuela",
@@ -73,8 +73,54 @@ const teachers = {
   ],
 };
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
 const School = () => {
-  return <div>School</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 2.4,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="about"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="about">About</TabsTrigger>
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="teachers">Teachers</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div className="min-h-[70vh] w-full">
+            {/* about */}
+            <TabsContent value="about" className="w-full">
+              about 
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default School;
