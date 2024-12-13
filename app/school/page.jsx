d1eaa854a46ffcb35d68227d3a55ce6a70d1437a
Andres <aaplv2@gmail.com>
2024-12-13 15:59:11 -0300

    esceual -> about

--- app/school/page.jsx
+++ app/school/page.jsx
@@ -105,9 +105,9 @@ const School = () => {
           className="flex flex-col xl:flex-row gap-[60px]"
         >
           <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
-            <TabsTrigger value="experience">Experience</TabsTrigger>
-            <TabsTrigger value="teachers">Teachers</TabsTrigger>
-            <TabsTrigger value="about">About</TabsTrigger>
+            <TabsTrigger value="experience">Experiencia</TabsTrigger>
+            <TabsTrigger value="teachers">Profesores</TabsTrigger>
+            <TabsTrigger value="about">Acerca</TabsTrigger>
           </TabsList>
 
           {/* content */}
@@ -137,6 +137,7 @@ const School = () => {
                 </ScrollArea>
               </div>
             </TabsContent>
+
             {/* teachers */}
             <TabsContent value="teachers" className="w-full">
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
@@ -167,9 +168,31 @@ const School = () => {
                 </ScrollArea>
               </div>
             </TabsContent>
+
             {/* about */}
-            <TabsContent value="about" className="w-full">
-              about
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center xl:justify-start gap-4"
+                      >
+                        <span className="text-white/60">{item.fieldName}</span>
+                        <span className="text-xl">{item.fieldValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
