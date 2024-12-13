435ca642b200f7edc45306608361b301d3b9f945
Andres <aaplv2@gmail.com>
2024-12-13 15:34:19 -0300

    escuela -> teachers

--- app/school/page.jsx
+++ app/school/page.jsx
@@ -139,7 +139,33 @@ const School = () => {
             </TabsContent>
             {/* teachers */}
             <TabsContent value="teachers" className="w-full">
-              teachers
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-black">{teachers.title}</h3>
+                <p className="max-w-[600px] text-wrap/60 mx-auto xl:mx-0">
+                  {teachers.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {teachers.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-5 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.experience}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.name}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60 ">{item.dances}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             {/* about */}
             <TabsContent value="about" className="w-full">
